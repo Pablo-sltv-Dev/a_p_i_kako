595d22c6cc324bb0e0b854931c9febdede4279ad
Pablo-sltv-Dev <pablo.pmlf@gmail.com>
2026-02-25 15:16:26 -0300

    esqueci de tirar o if

--- run.py
+++ run.py
@@ -106,15 +106,15 @@ def rsp():
 
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
     
-    if sucess:
-        app.run(
-            host='0.0.0.0',
-            port= sucess['port'],
-            debug=sucess['debug']
-        )
-    else:
-        raise ValueError("\nVerifique suas configurações no arquivo .env\n")
-        # print("Verifique suas configurações no arquivo .env")
+#     if sucess:
+#         app.run(
+#             host='0.0.0.0',
+#             port= sucess['port'],
+#             debug=sucess['debug']
+#         )
+#     else:
+#         raise ValueError("\nVerifique suas configurações no arquivo .env\n")
+#         # print("Verifique suas configurações no arquivo .env")
