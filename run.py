b0d1666e944ae07abf03582cd6a1724d6aa6f658
Pablo-sltv-Dev <pablo.pmlf@gmail.com>
2026-02-25 12:06:45 -0300

    o claude falou para eu tentar tirar o if do run.py

--- run.py
+++ run.py
@@ -2,18 +2,18 @@ from api import app, sucess
 
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
     
-    if sucess:
-        app.run(
-            host='0.0.0.0',
-            port= sucess['port'],
-            debug=sucess['debug']
-        )
-    else:
-        raise ValueError("\nVerifique suas configurações no arquivo .env\n")
-        # print("Verifique suas configurações no arquivo .env")
+#     if sucess:
+#         app.run(
+#             host='0.0.0.0',
+#             port= sucess['port'],
+#             debug=sucess['debug']
+#         )
+#     else:
+#         raise ValueError("\nVerifique suas configurações no arquivo .env\n")
+#         # print("Verifique suas configurações no arquivo .env")
 
 
 
