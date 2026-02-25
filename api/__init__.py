6884a56998a1cb67cdacee8198ba975ec3d7071a
Pablo-sltv-Dev <pablo.pmlf@gmail.com>
2026-02-25 11:47:28 -0300

    retirei o before do __init__ pq na minha cabeça fazia sentido, mas acabou que nem fazia

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,6 @@
 #bibliotecas:
 
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask_cors import CORS
 
 #______________________________
@@ -25,14 +25,6 @@ from .config import *
 from .db import *
 from .models import *
 
-@app.before_request
-def vrf_point():
-    try:
-        if not request.endpoint:
-            raise ModuleNotFoundError
-    except ModuleNotFoundError as error:
-        print(f"\n__ Endpoint não encontrado__\n {error}")
-        return jsonify({"Menssage": "endpoint não encontrado"})
 
 # except ImportError:
 #     from .routes import *
